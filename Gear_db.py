c6914c3f9d71310ae5a6802766daa9f27def4722
Onji <a.jonatan.fredriksson@gmail.com>
2022-07-11 23:16:42 +0800

    Built functionality to extract job gear and materia matrices frmo database. Updated get file for db to include Materia sockets

--- Gear_db.py
+++ Gear_db.py
@@ -1,88 +1,152 @@
 import numpy as np
 import json
 
-
-
 def Main():
-    database = load_json()
-    return database
+    Gear_set = Gearset('GNB')
+    Gear_ID = [2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    #Gear_set.show_JobGear()
 
-def load_json():
-    with open('database.json') as file:
-        database = json.load(file)
-    return database
+    Gear_set(Gear_ID)
+    Gear_set.show_stats()
 
-def get_JobGear(Job, database):
-    JobGear = {}
-    for key, val in database.items():
-        if Job in val['Jobs']:
-            JobGear.update({key:val})
-    return JobGear
 
-class Gear_base():
-    def __init__(self):
-        self.Strength = 0
-        self.Dexterity = 0
-        self.Vitality = 0
-        self.Intelligence = 0
-        self.Mind = 0
-        self.CriticalHit = 0
-        self.Determination = 0
-        self.DirectHitRate = 0
-        self.Defense = 0
-        self.MagicDefense = 0
-        self.AttackPower = 0
-        self.SkillSpeed = 0
+class Gearset():
 
-    def __add__(self, other):
-        rv1 = self.__dict__.copy()
-        for key in other.__dict__.keys():
-            rv1.update({key:self.__dict__.get(key, 0) + other.__dict__.get(key, 0)})
-        return rv
-
-class Gearset_base():
-    def __init__(self):
+    def __init__(self, Job):
+        self.AllowedMateria = [{},{},{},{},{},{},{},{},{},{},{}]
         self.Materia_Matrix = np.zeros([7, 7])
         self.ID = None
         self.slots = {'Weapon':None,
                     'Head':None,
-                    'Chest':None,
+                    'Body':None,
                     'Hands':None,
                     'Legs':None,
                     'Feet':None,
-                    'Ear':None,
-                    'Neck':None,
-                    'Braclet':None,
+                    'Earrings':None,
+                    'Necklace':None,
+                    'Bracelets':None,
                     'Ring1':None,
                     'Ring2':None}
-        self.options = self.slots.copy()
+
+        self.JobGear = self.slots.copy()
+
+        with open('database.json') as file:
+            self.database = json.load(file)
+
+        self.get_JobGear(Job)
 
     def __call__(self, Gear_ID):
-        assert len(Gear_ID) == 11, 'Gear ID must be a list of length 11'
-        self.ID = Gear_ID
+        Weapon, Head, Chest, Hands, Legs, Feet, Ear, Neck, Bracelet, Ring1, Ring2 = Gear_ID
+        self.ID = f"{Weapon}{Head}{Chest}{Hands}{Legs}{Feet}{Ear}{Neck}{Bracelet}{Ring1}{Ring2}"
 
-        for key, ID in zip(self.slots.keys(), Gear_ID):
-            self.slots.update({key:self.options[key][ID].name})
+        Choice = {'Weapon':Weapon, 
+                'Head':Head, 
+                'Body':Chest, 
+                'Hands':Hands, 
+                'Legs':Legs, 
+                'Feet':Feet, 
+                'Earrings':Ear, 
+                'Necklace':Neck, 
+                'Bracelets':Bracelet, 
+                'Ring1':Ring1,
+                'Ring2':Ring2}
+
+        i = 0
+        for Slot, ID in Choice.items():
+            SaveSlot = Slot
+            if 'Ring' in Slot:
+                Slot = 'Ring'
+
+            item = self.JobGear[Slot][ID].copy()
+            self.slots[SaveSlot] = item.pop('Name')
+            self.AllowedMateria[i].update(item.pop('AllowedMateria'))
+            i += 1
+
+            for stat, val in item.items():
+                setattr(self, stat, self.__dict__.get(stat, 0) + val)
+
+        self.iLVL = int(self.iLVL / 11)
+                    
+    def show_stats(self):
+        for key, val in self.slots.items():
+            print(f"{key:>10}:{val}")
+
+        print('---Gearset Stats---')
+        info = self.__dict__
+        info.pop('slots')
+        info.pop('database')
+        info.pop('JobGear')
+
+        print('Allowed Materia')
+        for i in info.pop('AllowedMateria'):
+            print(i)
 
 
-class Fending_gear(Gearset_base):
-    pass
+        for key, val in info.items():
+            print(f"{key:15}: {val}")
 
-class Healing_gear(Gearset_base):
-    pass
+    def get_JobGear(self, Job):
+        JobGear = {}
+        for key, val in self.database.items():
+            if Job in val['Jobs']:
 
-class Striking_gear(Gearset_base):
-    pass
+                if 'Asphodelos' in key:
+                    slot = 1
+                elif 'Augmented' in key:
+                    slot = 2
+                else:
+                    slot = 3
 
-class Maiming_gear(Gearset_base):
-    pass
+                Type = val['Type']
+                if 'Arm' in Type:
+                    Type = 'Weapon'
 
-class Aiming_gear(Gearset_base):
-    pass
+                Entry = {'Name':key}
+                Entry.update(val)
+                Entry.pop('Type')
+                Entry.pop('Jobs')
 
-class Casting_gear(Gearset_base):
-    pass
+                #Calculate materia matrix
+                Materia_value = 30
+                default_val = -1
+                MatStats = {'Critical Hit':Entry.get('Critical Hit', default_val),
+                            'Determination':Entry.get('Determination', default_val),
+                            'Direct Hit':Entry.get('Direct Hit', default_val),
+                            'Skill Speed':Entry.get('Skill Speed', default_val),
+                            'Spell Speed':Entry.get('Spell Speed', default_val),
+                            'Tenacity':Entry.get('Tenacity', default_val),
+                            'Piety':Entry.get('Piety', default_val)}
 
+                Stat_cap = max(MatStats.values())
+                for mkey, mval in MatStats.items():
+                    MatStats.update({mkey:min(int((Stat_cap - mval)/Materia_value), val['Materia_Sockets'])})
+                Entry.update({'AllowedMateria':MatStats})
+
+                if val['Materia_Sockets'] > 0:
+                    A = np.ones([7, 7])
+                    b = list(MatStats.values())
+                    bA = b*A
+                    Materia_Matrix = (bA + np.transpose(bA))
+                    np.fill_diagonal(Materia_Matrix, b)
+                    Materia_Matrix.clip(0, val['Materia_Sockets'])
+
+                    print(Materia_Matrix)
+                    Entry.update({'Materia_Matrix':Materia_Matrix})
+
+
+                try:
+                    JobGear[Type].update({slot:Entry})
+                except:
+                    JobGear.update({Type:{slot:Entry}})
+
+        self.JobGear = JobGear
+
+    def show_JobGear(self):
+        for key, val in self.JobGear.items():
+            print(' ')
+            print(key)
+            for subkey, subval in val.items():
+                print(f"    {subkey}: {subval['Name']}")
 
 
 
--- get_Geardata.py
+++ get_Geardata.py
@@ -1,79 +1,101 @@
 from bs4 import BeautifulSoup as bfs
+import lxml
 import requests
 from tqdm import tqdm
 import json
 
-iLVL = 600
 
-All_gear_url = {'Gear 1':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&min_item_lv={iLVL}",
-				'Gear 2': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&page=2&min_item_lv={iLVL}",
-				'Weapons 1': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=1&min_item_lv={iLVL}",
-				'Weapons 2': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=1&page=2&min_item_lv={iLVL}"}
+def main():
+	UpdateData(600)
+	#developer('https://na.finalfantasyxiv.com/lodestone/playguide/db/item/f44294126eb/')
+	#developer('https://na.finalfantasyxiv.com/lodestone/playguide/db/item/b71da5422ab/')
 
-with open('All_gear_url.json', 'w') as file:
-	json.dump(All_gear_url, file)
 
-Gear_urls = {'Head':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=34&min_item_lv={iLVL}",
-			'Chest':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=35&min_item_lv={iLVL}",
-			'Hands':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=37&min_item_lv={iLVL}",
-			'Legs':	f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=36&min_item_lv={iLVL}",
-			'Feet':	f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=38&min_item_lv={iLVL}",
-			'Shields':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=11&min_item_lv={iLVL}",
-			'Earrings':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=41&min_item_lv={iLVL}",
-			'Necklace':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=40&min_item_lv={iLVL}",
-			'Bracelets':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=42&min_item_lv={iLVL}",
-			'Ring': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=43&min_item_lv={iLVL}"
-			}
-with open('Gear_urls.json', 'w') as file:
-	json.dump(Gear_urls, file)
+def UpdateData(iLVL):
+	All_gear_url = {'Gear 1':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&min_item_lv={iLVL}",
+					'Gear 2': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&page=2&min_item_lv={iLVL}",
+					'Accessories': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&min_item_lv={iLVL}",
+					'Weapons 1': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=1&min_item_lv={iLVL}",
+					'Weapons 2': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=1&page=2&min_item_lv={iLVL}"}
 
-url_base = "https://na.finalfantasyxiv.com"
+	with open('All_gear_url.json', 'w') as file:
+		json.dump(All_gear_url, file)
 
-with open('url_base.json', 'w') as file:
-	json.dump(url_base, file)
+	Gear_urls = {'Head':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=34&min_item_lv={iLVL}",
+				'Chest':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=35&min_item_lv={iLVL}",
+				'Hands':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=37&min_item_lv={iLVL}",
+				'Legs':	f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=36&min_item_lv={iLVL}",
+				'Feet':	f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=38&min_item_lv={iLVL}",
+				'Shields':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3&category3=11&min_item_lv={iLVL}",
+				'Earrings':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=41&min_item_lv={iLVL}",
+				'Necklace':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=40&min_item_lv={iLVL}",
+				'Bracelets':f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=42&min_item_lv={iLVL}",
+				'Ring': f"https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=4&category3=43&min_item_lv={iLVL}"
+				}
+	with open('Gear_urls.json', 'w') as file:
+		json.dump(Gear_urls, file)
 
-#---Load urls to all gear---
-Links = {}
-for key, val in tqdm(All_gear_url.items()):
-	source = requests.get(val).text
+	url_base = "https://na.finalfantasyxiv.com"
+
+	with open('url_base.json', 'w') as file:
+		json.dump(url_base, file)
+
+	#---Load urls to all gear---
+	Links = {}
+	for key, val in tqdm(All_gear_url.items()):
+		source = requests.get(val).text
+		soup = bfs(source, 'lxml')
+		Gear = soup.find_all('a', class_='db_popup db-table__txt--detail_link')
+
+		for g in Gear: 
+			Link = url_base + g.get('href')
+			Name = g.text
+			Links.update({Name:Link})
+
+	with open('Links.json', 'w') as file:
+		json.dump(Links, file)
+
+	#---Load data from gear page---
+	#url = "https://na.finalfantasyxiv.com/lodestone/playguide/db/item/39b5b3b6f4e/"
+
+	database = {}
+	for key, val in tqdm(Links.items()):
+
+		source = requests.get(val).text
+		soup = bfs(source, 'lxml')
+		data = {}
+		
+		data.update({'Type':soup.find('p', class_="db-view__item__text__category").text,
+					'iLVL':int(soup.find('div', class_="db-view__item_level").text.split('Item Level ')[1]),
+					'Jobs':soup.find('div', class_="db-view__item_equipment__class").text,
+					'Materia_Sockets': str(soup.find('ul', class_='db-view__materia_socket')).count('socket normal')
+					})
+		
+		Bonuses = soup.find('ul', class_="db-view__basic_bonus").text
+		Split = Bonuses.split('\n')
+		Split.pop(0)
+		Split.pop(-1)
+		
+		for info in Split:
+			stat, val = info.split(' +')
+			data.update({stat:int(val)})
+		
+		database.update({key:data})
+
+	with open('database.json', 'w') as file:
+		json.dump(database, file)
+
+	print('Update completed, json files updated!')
+
+
+
+def developer(url):
+	print('Dev testing...')
+	source = requests.get(url).text
 	soup = bfs(source, 'lxml')
-	Gear = soup.find_all('a', class_='db_popup db-table__txt--detail_link')
 
-	for g in Gear: 
-		Link = url_base + g.get('href')
-		Name = g.text
-		Links.update({Name:Link})
+	sockets = str(soup.find('ul', class_='db-view__materia_socket')).count('socket normal')
+	print(sockets)
 
-with open('Links.json', 'w') as file:
-	json.dump(Links, file)
-
-#---Load data from gear page---
-#url = "https://na.finalfantasyxiv.com/lodestone/playguide/db/item/39b5b3b6f4e/"
-
-database = {}
-for key, val in tqdm(Links.items()):
-
-	source = requests.get(val).text
-	soup = bfs(source, 'lxml')
-	data = {}
-	
-	data.update({'Type':soup.find('p', class_="db-view__item__text__category").text,
-				'iLVL':int(soup.find('div', class_="db-view__item_level").text.split('Item Level ')[1]),
-				'Jobs':soup.find('div', class_="db-view__item_equipment__class").text
-				})
-	
-	Bonuses = soup.find('ul', class_="db-view__basic_bonus").text
-	Split = Bonuses.split('\n')
-	Split.pop(0)
-	Split.pop(-1)
-	
-	for info in Split:
-		stat, val = info.split(' +')
-		data.update({stat:int(val)})
-	
-	database.update({key:data})
-
-with open('database.json', 'w') as file:
-	json.dump(database, file)
-
-print('Update completed, json files updated!')
+if __name__ == '__main__':
+	main()
